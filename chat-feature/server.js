5b6b4770c8bccb8579740e8c4f8d47a24cc38505
Javier Prieto-Duenas <pprieto9969@hotmail.com>
2023-04-17 21:15:52 -0400

    up

---
+++ chat-feature/server.js
@@ -0,0 +1,14 @@
+const path = require('path');
+const http = require('http');
+const express = require('express');
+
+const app = express();
+const server = http.createServer(app);
+
+//Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+const PORT = 3000 || process.env.PORT;
+
+server.listen(PORT, () => console.log(`Server running on ${PORT}`));
+https://www.youtube.com/watch?v=jD7FnbI76Hg
