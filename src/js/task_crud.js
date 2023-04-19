cd400fffb4da7781acc4cd88ddc522ea9b162353
Javier Prieto <pprieto9969@hotmail.com>
2023-04-19 11:00:26 -0400

    up

---
+++ src/js/task_crud.js
@@ -0,0 +1,16 @@
+const path = require("path");
+const express = require("express");
+const mysql = require("mysql");
+const ap = express();
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+
+ap.get('/', )
+
+
+
+//Server listening
+ap.listen(2998, () => {
+    console.log("Server is running on port 2999")
+});
+
